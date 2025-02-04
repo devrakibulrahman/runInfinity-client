8a0bd20ec99ce6bd42c4aabb884a6d67d0fea791
devrakibulrahman <devrakibulrahman@gmail.com>
2025-02-04 14:13:14 +0600

    modify regsiter and login page design

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,7 +4,7 @@ const RegisterPage = () => {
     return (
         <>
             <div className="w-full min-h-screen bg-[url(/img/register.png)] bg-fit flex items-center justify-center">
-                <div className="container mx-auto px-4">
+                <div className="container mx-auto px-4 flex items-center justify-center">
                     <div className="w-[450px] min-h-[500px] bg-white p-6 dark:bg-gray-950">
                         <div className="w-full">
                             <h1 className="poppins text-3xl text-black font-bold uppercase dark:text-white">Register</h1>
