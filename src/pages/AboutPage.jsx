4ff5bc18e44a814d1a70f83d9f12dcf38897ff47
devrakibulrahman <devrakibulrahman@gmail.com>
2025-02-05 22:45:14 +0600

    added about us section in the about us page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,9 +6,9 @@ const AboutPage = () => {
         <>
             <div className="w-full min-h-screen">
                 <PageHeading image={'/img/aboutus.png'} heading={'About Us'} paragraph={'Welcome to our Marathon Management platform, dedicated to organizing seamless, efficient, and inspiring running events worldwide.'}></PageHeading>
-                {/* <div className="w-full min-h-[200px]">
+                <div className="w-full min-h-[200px]">
                     <AboutSection></AboutSection>
-                </div> */}
+                </div>
             </div>
         </>
     );
