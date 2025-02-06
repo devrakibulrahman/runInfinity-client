ef44f25a437e44901f1e03d257fb42c17fea0a7c
devrakibulrahman <devrakibulrahman@gmail.com>
2025-02-06 13:12:25 +0600

    added page heading in the marathon details page

--- src/pages/MarathonDetailsPage.jsx
+++ src/pages/MarathonDetailsPage.jsx
@@ -1,8 +1,11 @@
+import PageHeading from "../components/PageHeading";
 
 const MarathonDetailsPage = () => {
     return (
         <>
-            
+            <div className="w-full min-h-screen">
+                <PageHeading image={'/img/marathon_details.png'} heading={'Marathon Details'} paragraph={'Join us for an exciting marathon challenge, featuring scenic routes, race kits, medals, and prizes!'}></PageHeading>
+            </div>
         </>
     );
 };
