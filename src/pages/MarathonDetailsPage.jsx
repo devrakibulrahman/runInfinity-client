5b203295e8fca4da6910b26d23dd7051fccc10d0
devrakibulrahman <devrakibulrahman@gmail.com>
2025-02-06 21:29:00 +0600

    added join us section in the marathon details page

--- src/pages/MarathonDetailsPage.jsx
+++ src/pages/MarathonDetailsPage.jsx
@@ -1,3 +1,5 @@
+import BlogSection from "../components/BlogSection";
+import JoinUsSection from "../components/JoinUsSection";
 import MarathonDetailsSection from "../components/MarathonDetailsSection";
 import PageHeading from "../components/PageHeading";
 
@@ -9,6 +11,12 @@ const MarathonDetailsPage = () => {
                 <div className="w-full min-h-[200px] bg-white dark:bg-gray-900">
                     <MarathonDetailsSection></MarathonDetailsSection>
                 </div>
+                <div className="w-full min-h-[200px] bg-orange-50/50 dark:bg-gray-950">
+                    <JoinUsSection></JoinUsSection>
+                </div>
+                {/* <div className="w-full min-h-[200px] bg-white dark:bg-gray-900">
+                    <BlogSection></BlogSection>
+                </div> */}
             </div>
         </>
     );
