bb0bce3598788a05de11b75b0a76b3cf983b171f
devrakibulrahman <devrakibulrahman@gmail.com>
2025-02-07 20:08:18 +0600

    update the route in marathon registration

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
                 element: <MarathonDetailsPage></MarathonDetailsPage>,
             },
             {
-                path: '/marathons/registration/:id',
+                path: '/marathons/marathon_details/registration/:id',
                 element: <RegistrationPage></RegistrationPage>
             },
             {
